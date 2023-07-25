2a2e82947fc20fc12fb8e0e1ee5c2f93e6ec3381
CodingMasterLSW <108395670+CodingMasterLSW@users.noreply.github.com>
2023-07-26 00:49:02 +0900

    first-setting

---
+++ src/main/java/co_2/suggest_project/SuggestProjectApplication.java
@@ -0,0 +1,13 @@
+package com2.suggest_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuggestProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuggestProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co_2/suggest_project/SuggestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com2.suggest_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuggestProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
