7e656f486b84dc6f4f893dd02b160be45b18a6cd
CodingMasterLSW <108395670+CodingMasterLSW@users.noreply.github.com>
2023-07-26 01:20:23 +0900

    print index

--- src/main/java/co_2/suggest_project/SuggestProjectApplication.java
+++ src/main/java/co_2/suggest_project/SuggestProjectApplication.java
@@ -1,9 +1,10 @@
-package com2.suggest_project;
+package co_2.suggest_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class SuggestProjectApplication {
 
 	public static void main(String[] args) {
