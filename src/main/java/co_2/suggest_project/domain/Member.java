d2b4406217fcaa7e7cdaa333fd1a72707d39b843
[jina4066] <[jina4066@naver.com]>
2023-08-12 18:44:36 +0900

    feat : 회원 탈퇴 및 정보 update 구현

--- src/main/java/co_2/suggest_project/domain/Member.java
+++ src/main/java/co_2/suggest_project/domain/Member.java
@@ -11,6 +11,7 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Entity
 @Getter
@@ -33,10 +34,12 @@ public class Member { //회원 엔티티
   @Column(length = 100)
   private String role;
 
-  @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
+
+  //== 회원탈퇴 -> 작성한 게시물, 댓글 모두 삭제 ==//
+  @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
   private List<Post> posts = new ArrayList<>();
 
-  @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
+  @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
   private List<Comment> comments = new ArrayList<>();
 
   // 게시물 추가,삭제 메소드
@@ -59,5 +62,29 @@ public class Member { //회원 엔티티
   public void removeComment(Comment comment) {
     comments.remove(comment);
     comment.setMember(null);
+  }
+
+  //== 정보 수정 ==//
+  public void updatePassword(PasswordEncoder passwordEncoder, String password){
+    this.password = passwordEncoder.encode(password);
+  }
+
+  public void updateNickName(String nickname){
+    this.nickname = nickname;
+  }
+
+  //토큰 업데이트 관련
+//  public void updateRefreshToken(String refreshToken){
+//    this.refreshToken = refreshToken;
+//  }
+//  public void destroyRefreshToken(){
+//    this.refreshToken = null;
+//  }
+
+
+
+  //== 패스워드 암호화 ==//
+  public void encodePassword(PasswordEncoder passwordEncoder){
+    this.password = passwordEncoder.encode(password);
   }
 }
